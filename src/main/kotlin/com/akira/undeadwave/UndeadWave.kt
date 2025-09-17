7c0e0d8d00b1f6cec3f109cebbaeeed2a9d9357c
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-17 10:32:46 +0800

    完成主类基础部分。

--- src/main/kotlin/com/akira/undeadwave/UndeadWave.kt
+++ src/main/kotlin/com/akira/undeadwave/UndeadWave.kt
@@ -2,5 +2,13 @@ package com.akira.undeadwave
 
 import com.akira.core.api.AkiraPlugin
 
-class UndeadWave: AkiraPlugin() {
+class UndeadWave : AkiraPlugin() {
+    companion object {
+        lateinit var instance: UndeadWave
+            private set
+    }
+
+    init {
+        instance = this
+    }
 }
