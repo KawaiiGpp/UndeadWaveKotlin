d21cd106fc929beef34646ebd47a8b3ce023aa54
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-10-10 15:54:56 +0800

    为ArenaSession新增操作kills/coins/score的方法。

--- src/main/kotlin/com/akira/undeadwave/main/arena/ArenaSession.kt
+++ src/main/kotlin/com/akira/undeadwave/main/arena/ArenaSession.kt
@@ -7,6 +7,22 @@ class ArenaSession(
     val difficulty: ArenaDifficulty
 ) {
     var kills = 0
+        private set
     var coins = 0
-    var score = 0
+        private set
+    var score = 0.0
+        private set
+
+    fun increaseKills() = let { kills += 1 }
+
+    fun gainCoins(amount: Int) = amount.let { coins += it }
+
+    fun costCoins(amount: Int): Boolean {
+        if (amount <= coins) {
+            coins -= amount
+            return true
+        } else return false
+    }
+
+    fun gainScore(amount: Double) = amount.let { score += (it * difficulty.scoreMultiplier) }
 }
