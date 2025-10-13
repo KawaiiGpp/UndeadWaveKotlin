e418f669a10c5f5bfb43c3983cfe553e3cc2c054
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-10-13 13:47:43 +0800

    为ArenaDifficulty新增全局标识符属性。

--- src/main/kotlin/com/akira/undeadwave/main/arena/ArenaDifficulty.kt
+++ src/main/kotlin/com/akira/undeadwave/main/arena/ArenaDifficulty.kt
@@ -12,5 +12,7 @@ enum class ArenaDifficulty(
 ) {
     EASY("简单", NamedTextColor.DARK_GREEN, 1.5, 0.75, 0.8, 0.5),
     NORMAL("普通", NamedTextColor.GOLD, 1.0, 1.0, 1.0, 1.0),
-    HARD("困难", NamedTextColor.DARK_RED, 0.75, 1.5, 1.25, 2.0)
+    HARD("困难", NamedTextColor.DARK_RED, 0.75, 1.5, 1.25, 2.0);
+
+    val identifier get() = name.lowercase()
 }
