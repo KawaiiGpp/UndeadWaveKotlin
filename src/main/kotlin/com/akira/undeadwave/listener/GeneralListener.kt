56d803988152b1d18ec5b771ae6e7e67f98bbab0
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-10-13 17:46:08 +0800

    把ArenaPresetResetListener合并进GeneralListener。

--- src/main/kotlin/com/akira/undeadwave/listener/GeneralListener.kt
+++ src/main/kotlin/com/akira/undeadwave/listener/GeneralListener.kt
@@ -1,11 +1,20 @@
 package com.akira.undeadwave.listener
 
 import com.akira.undeadwave.main.arena.Arena
+import com.akira.undeadwave.main.arena.ArenaPreset
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
 
 class GeneralListener : Listener {
     @EventHandler
-    fun onQuit(event: PlayerQuitEvent) = Arena.PlayerMap.get(event.player)?.shutdown(false)
+    fun onQuit(event: PlayerQuitEvent) {
+        val player = event.player
+        val uniqueId = player.uniqueId
+
+        Arena.PlayerMap.get(player)?.shutdown(false)
+
+        if (!ArenaPreset.Creator.isRegistered(uniqueId)) return
+        ArenaPreset.Creator.unregister(uniqueId)
+    }
 }
