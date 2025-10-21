53135f7eb26b8903ad3ff4680aee868974c89f67
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-10-21 14:53:02 +0800

    PlayerExtension改为GeneralExtension并新增Entity.lastDamager。

--- src/main/kotlin/com/akira/undeadwave/util/GeneralExtension.kt
+++ src/main/kotlin/com/akira/undeadwave/util/GeneralExtension.kt
@@ -4,7 +4,9 @@ import com.akira.core.api.util.entity.resetMaxHealthModifiers
 import com.akira.core.api.util.entity.setBaseMaxHealth
 import net.kyori.adventure.text.Component
 import org.bukkit.GameMode
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 
 fun Player.restore() {
     resetMaxHealthModifiers()
@@ -23,3 +25,5 @@ fun Player.restore() {
     health = 20.0
     foodLevel = 20
 }
+
+val Entity.lastDamager get() = (lastDamageCause as? EntityDamageByEntityEvent)?.damager
